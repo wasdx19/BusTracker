c81ce0b53c1708d93ba14232e1914a696b0c2a36
wasdx19 <vanrwei@gmail.com>
2021-06-13 20:32:26 +0600

    NotifFixes

--- app/src/main/java/com/example/busstopreminder/service/BusTrackerService.kt
+++ app/src/main/java/com/example/busstopreminder/service/BusTrackerService.kt
@@ -1,9 +1,13 @@
 package com.example.busstopreminder.service
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Handler
 import android.os.IBinder
 import android.widget.Toast
@@ -62,11 +66,25 @@ class BusTrackerService : Service() {
         builder.setOngoing(true)
         builder.setSubText(subText) //API level 16
         builder.setVibrate(longArrayOf( 1000, 3000, 1000, 3000, 1000, 3000, 1000 ))
-
         builder.setNumber(100)
-        builder.build()
 
-        val notif = builder.notification
-        notifManager.notify(11, notif)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "Bus Reminder",
+                "BuStopReminder",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channel.description = "CHANNEL_DESC"
+            channel.setShowBadge(true)
+            channel.canShowBadge()
+            channel.enableLights(true)
+            channel.lightColor = Color.RED
+            channel.enableVibration(true)
+            channel.vibrationPattern = longArrayOf(1000, 3000, 1000, 3000, 1000, 3000, 1000 )
+
+            notificationManager.createNotificationChannel(channel)
+        }
+        notifManager.notify(11, builder.build())
     }
 }
